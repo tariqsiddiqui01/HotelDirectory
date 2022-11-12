eea62d0c54496fc687ce15d241292adac15973da
Hp_15s <Hp_15s@DESKTOP-I1NE26U>
2022-11-12 13:39:00 +0530

    Add project files.

---
+++ HotelListing.API/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace HotelListing.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).UseSerilog((ctx, lc) =>
+            lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
